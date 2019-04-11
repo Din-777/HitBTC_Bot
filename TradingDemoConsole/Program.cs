8eb5a651c81baab571caa9b28839d52fe3c6d36e
Din-777 <din_777@mail.ru>
2019-04-11 16:51:02 +0500

    test

--- TradingDemoConsole/Program.cs
+++ TradingDemoConsole/Program.cs
@@ -11,6 +11,19 @@ namespace TradingDemoConsole
 {
 	class Program
 	{
+		public class Dealing
+		{
+			public string tred; // "buy" "sel"
+			public float price;
+
+			public Dealing(string tred, float price)
+			{
+				this.tred = tred;
+				this.price = price;
+			}
+
+		}
+
 		public struct Balance
 		{
 			public float USD;
@@ -18,10 +31,14 @@ namespace TradingDemoConsole
 
 			public float estimatedUSD;
 			public float estimatedBTC;
-		}
 
+			public Stack<Dealing> deals;
+		}
+				
 		public class Trading
-		{
+		{			
+			public List<Dealing> OpenDeals;
+
 			private static Ticker oldTicker;
 
 			public float TradUSD { get; set; }
@@ -32,9 +49,10 @@ namespace TradingDemoConsole
 
 			public Trading()
 			{
+				OpenDeals = new List<Dealing>();
 			}
 
-			public void trading(ref Balance balance, Ticker ticker)
+			public void trading_1(ref Balance balance, Ticker ticker)
 			{
 				this.ticker = ticker;
 
@@ -44,6 +62,53 @@ namespace TradingDemoConsole
 
 					if (ticker.bid > oldTicker.bid) selBTC(ref balance);
 				}
+
+				oldTicker = ticker;
+			}
+
+			private int h = 0;
+			public void trading_2(ref Balance balance, Ticker ticker)
+			{
+				this.ticker = ticker;
+				List<Dealing> tempOpenDeals = new List<Dealing>();
+
+				if (oldTicker == null)
+				{
+					buyBTC(ref balance);
+					OpenDeals.Add(new Dealing("buy", ticker.ask));
+					balance.deals.Push(new Dealing("buy", ticker.ask));
+
+					selBTC(ref balance);
+					OpenDeals.Add(new Dealing("sel", ticker.bid));
+					balance.deals.Push(new Dealing("sel", ticker.bid));
+					h = 1;
+				}
+				else
+				{	
+					foreach (Dealing d in OpenDeals)
+					{
+						if(d.tred == "buy")
+						{
+							if (ticker.bid > d.price)
+							{
+								selBTC(ref balance);
+								tempOpenDeals.Add(new Dealing("sel", ticker.bid));
+								balance.deals.Push(new Dealing("sel", ticker.bid));
+							}
+						}
+						if (d.tred == "sel")
+						{
+							if (ticker.ask < d.price)
+							{
+								buyBTC(ref balance);
+								tempOpenDeals.Add(new Dealing("buy", ticker.ask));
+								balance.deals.Push(new Dealing("buy", ticker.ask));
+							}
+						}
+					}
+				}
+
+				OpenDeals = OpenDeals.Concat<Dealing>(tempOpenDeals).ToList<Dealing>();
 
 				oldTicker = ticker;
 			}
@@ -67,30 +132,46 @@ namespace TradingDemoConsole
 			}
 		}
 
-		static void printScreen(Balance balance, Stack<float> stack)
+
+		static void printScreen(Balance balance, Stack<float> prices)
 		{
 			Console.Clear();
 			Console.Title = string.Format("BTC/USD {0:0000.000}     Initial estimated USD {1:000.000000}",
-				stack.Peek(), 100.0f + (0.01f * stack.Last<float>()));
+				prices.Peek(), 100.0f + (0.01f * prices.Last<float>()));
 
-			Console.SetCursorPosition(22, 0);
-			Console.WriteLine("Trading balance USD {0:000.000}", balance.USD);
+			Console.SetCursorPosition(45, 0);
+			Console.WriteLine("Trading balance");
+			Console.SetCursorPosition(65, 0);
+			Console.WriteLine("Estimated balance");
 
-			Console.SetCursorPosition(38, 1);
-			Console.WriteLine("BTC {0:0.00000}", balance.BTC);
-
-
-			Console.SetCursorPosition(55, 0);
-			Console.WriteLine("Estimated USD {0:000.000000}", balance.estimatedUSD);
-
+			Console.SetCursorPosition(45, 1);
+			Console.WriteLine("USD {0:000.000000}", balance.USD);
+			Console.SetCursorPosition(45, 2);
+			Console.WriteLine("BTC {0:0.00000000}", balance.BTC);
+			
 			Console.SetCursorPosition(65, 1);
+			Console.WriteLine("USD {0:000.000000}", balance.estimatedUSD);
+			Console.SetCursorPosition(65, 2);
 			Console.WriteLine("BTC {0:0.00000000}", balance.estimatedBTC);
+
+			Console.SetCursorPosition(0, 0);
+			Console.Write("Prices");
+
+			Console.SetCursorPosition(15, 0);
+			Console.Write("Dealings");
 
 			for (int i = 0; i < 20; i++)
 			{
-				Console.SetCursorPosition(0, i);
-				Console.Write("{0:0000.000}", stack.ElementAtOrDefault<float>(i));
+				Console.SetCursorPosition(0, i+1);
+				Console.Write("{0:0000.000}", prices.ElementAtOrDefault<float>(i));
+
+				if (i < balance.deals.Count)
+				{
+					Console.SetCursorPosition(15, i+1);
+					Console.Write(balance.deals.ElementAtOrDefault<Dealing>(i).tred + " {0:0000.000}", balance.deals.ElementAtOrDefault<Dealing>(i).price);
+				}				
 			}
+
 
 		}
 				
@@ -98,9 +179,10 @@ namespace TradingDemoConsole
 		static void Main(string[] args)
 		{
 			Balance balance = new Balance();
+			balance.deals = new Stack<Dealing>();
 			Ticker ticker = new Ticker();
 			HBTC hitBtc = new HBTC();
-			Stack<float> stack = new Stack<float>();
+			Stack<float> prices = new Stack<float>();			
 			Trading trading = new Trading();
 
 			balance.USD = 100.0f;
@@ -115,13 +197,13 @@ namespace TradingDemoConsole
 			while (true)
 			{
 				string respons = hitBtc.Request(out ticker, Pair.BTCUSD);
-				stack.Push(Convert.ToSingle(ticker.ask));
+				prices.Push(Convert.ToSingle(ticker.ask));
 
-				trading.trading(ref balance, ticker);
+				trading.trading_2(ref balance, ticker);
 
 				trading.calcEstimete(ref balance, ticker);
 
-				printScreen(balance, stack);
+				printScreen(balance, prices);
 				Thread.Sleep(2000);
 
 			}
