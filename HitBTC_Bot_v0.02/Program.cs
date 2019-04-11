770af3170b3cfac8ad8fe0e995265ae40a68f801
Din-777 <din_777@mail.ru>
2019-04-11 08:23:01 +0500

    Добавьте файлы проекта.

---
+++ HitBTC_Bot_v0.02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HitBTC_Bot_v0._02
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
